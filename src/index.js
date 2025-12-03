c66fa6861fd359d74e14c07f5bb56b0917d5f953
Jrgamer4u <jrgamer4u@gmail.com>
2025-12-03 21:54:49 +0000

    tst

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { DotLottie } from '@lottiefiles/dotlottie-web';
+
+const dotLottie = new DotLottie({
+    autoplay: true,
+    loop: true,
+    canvas: document.querySelector('#dotlottie-canvas-1'),
+    src: "https://lottie.host/4db68bbd-31f6-4cd8-84eb-189de081159a/IGmMCqhzpt.lottie", // or .json file
+});
+
+const dotLottie2 = new DotLottie({
+    autoplay: true,
+    loop: true,
+    canvas: document.querySelector('#dotlottie-canvas-2'),
+    src: url("BrandIdentityToggle.lottie"), // or .json file
+});
